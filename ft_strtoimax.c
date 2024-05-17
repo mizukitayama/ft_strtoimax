05c587e4736fe57c9980666d1df894057e6dbc3a
Mizuki <tayama_mizuki@eagle.sophia.ac.jp>
2024-05-17 18:40:13 +0900

    norminette ok

--- ft_strtoimax.c
+++ ft_strtoimax.c
@@ -1,28 +1,7 @@
-// https://git.quacker.org/d/freebsd-nq/commit/9c5cbc30e7062561affda0e5413f6db27a83b27e#diff-59f10e04966536300a4a3ab9c1a13c389327d620
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <errno.h>
-
-static bool	ft_isupper(int c)
-{
-	return (c >= 'A' && c <= 'Z');
-}
-
-static bool	ft_isalpha(int c)
-{
-	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
-}
-
-static int	ft_digittoint(int c)
-{
-	return (c - '0');
-}
-
-static bool	ft_isdigit(int c)
-{
-	return ((c >= '0' && c <= '9'));
-}
 
 static bool	ft_isspace(int c)
 {
@@ -47,7 +26,7 @@ static int	is_negative(const char **s)
 	}
 	else if (**s == '+')
 	{
-			(*s)++;
+		(*s)++;
 	}
 	return (0);
 }
@@ -70,112 +49,152 @@ static int	parse_base(const char **s, int base)
 	return (base);
 }
 
+static int	get_digit_value(unsigned char c, int base)
+{
+	if ((c >= '0' && c <= '9'))
+		return (c - '0');
+	else if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		if ((c >= 'A' && c <= 'Z'))
+			return (c - 'A' + 10);
+		else
+			return (c - 'a' + 10);
+	}
+	return (-1);
+}
+
+static uintmax_t	parse_integer_core(const char **s, int base, int *any,
+	uintmax_t cutoff) {
+	uintmax_t			integer_part;
+	unsigned char		c;
+	int					digit_value;
+	uintmax_t			cutlim;
+
+	integer_part = 0;
+	cutlim = cutoff % base;
+	while (1)
+	{
+		c = **s;
+		digit_value = get_digit_value(c, base);
+		if (digit_value == -1 || digit_value >= base)
+			break ;
+		if (*any < 0 || (integer_part > cutoff
+				|| (integer_part == cutoff && digit_value > cutlim)))
+			*any = -1;
+		else
+		{
+			*any = 1;
+			integer_part = integer_part * base + digit_value;
+		}
+		(*s)++;
+	}
+	return (integer_part);
+}
+
+static uintmax_t	parse_integer(const char **s, int base, int neg, int *any)
+{
+	uintmax_t	cutoff;
+
+	if (neg)
+		cutoff = (uintmax_t) - (INTMAX_MIN + INTMAX_MAX) + INTMAX_MAX;
+	else
+		cutoff = INTMAX_MAX;
+	cutoff /= base;
+	return (parse_integer_core(s, base, any, cutoff));
+}
+
+static intmax_t	assign_intmax(int neg)
+{
+	if (neg)
+		return (INTMAX_MIN);
+	return (INTMAX_MAX);
+}
+
+static void	update_endptr(char **endptr, const char *s,
+	const char *nptr, int any)
+{
+	if (endptr == NULL)
+		return ;
+	if (any)
+		*endptr = (char *)s;
+	else
+		*endptr = (char *)nptr;
+}
+
 intmax_t	ft_strtoimax(const char *nptr, char **endptr, int base)
 {
 	const char		*s;
-	uintmax_t			acc;
-	unsigned char	c;
-	uintmax_t			cutoff;
-	int						neg;
-	int						any;
-	int						cutlim;
+	int				neg;
+	uintmax_t		integer_part;
+	int				any;
 
 	s = skip_whitespace(nptr);
 	neg = is_negative(&s);
 	base = parse_base(&s, base);
-	acc = 0;
 	any = 0;
 	if (base < 2 || base > 36)
 		errno = EINVAL;
-	cutoff = neg ? (uintmax_t) - (INTMAX_MIN + INTMAX_MAX) + INTMAX_MAX : INTMAX_MAX;
-	cutlim = cutoff % base;
-	cutoff /= base;
-	while (1)
-	{
-		c = *s;
-		if (ft_isdigit(c))
-			c = ft_digittoint(c);
-		else if (ft_isalpha(c))
-			c -= ft_isupper(c) ? 'A' - 10 : 'a' - 10;
-		else
-			break;
-		if (c >= base)
-		{
-			break;
-		}
-		if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
-		{
-			any = -1;
-		}
-		else
-		{
-			any = 1;
-			acc *= base;
-			acc += c;
-		}
-		s++;
-	}
+	integer_part = parse_integer(&s, base, neg, &any);
 	if (any < 0)
 	{
-		acc = neg ? INTMAX_MIN : INTMAX_MAX;
+		integer_part = assign_intmax(neg);
 		errno = ERANGE;
 	}
 	else if (!any)
 		errno = EINVAL;
 	else if (neg)
-		acc = -acc;
-	if (endptr != NULL)
-		*endptr = (char *)(any ? s : nptr);
-	return (acc);
+		integer_part = -integer_part;
+	update_endptr(endptr, s, nptr, any);
+	return (integer_part);
 }
 
-#include <inttypes.h>
-int main()
-{
-	char *s = "    321g2 ";
-	char *end = "";
-	printf("%jd\n", ft_strtoimax(s, &end, 4));
-	printf("end: %s\n", end);
-	printf("%jd\n", strtoimax(s, &end, 4));
-	printf("end: %s\n", end);
-	printf("\n============\n\n");
-	printf("%jd\n", ft_strtoimax(s, &end, 10));
-	printf("end: %s\n", end);
-	printf("%jd\n", strtoimax(s, &end, 10));
-	printf("end: %s\n", end);
-	printf("\n============\n\n");
-	s = "0x236f9   ";
-	printf("%jd\n", ft_strtoimax(s, &end, 15));
-	printf("end: %s\n", end);
-	printf("%jd\n", strtoimax(s, &end, 15));
-	printf("end: %s\n", end);
-	printf("\n============\n\n");
-	printf("%jd\n", ft_strtoimax(s, &end, 0));
-	printf("end: %s\n", end);
-	printf("%jd\n", strtoimax(s, &end, 0));
-	printf("end: %s\n", end);
-	printf("\n============\n\n");
-	s = "   -0x2A36E9   ";
-	printf("%jd\n", ft_strtoimax(s, &end, 16));
-	printf("end: %s\n", end);
-	printf("%jd\n", strtoimax(s, &end, 16));
-	printf("end: %s\n", end);
-	printf("\n============\n\n");
-	s = "		04434   ";
-	printf("%jd\n", ft_strtoimax(s, &end, 0));
-	printf("end: %s\n", end);
-	printf("%jd\n", strtoimax(s, &end, 0));
-	printf("end: %s\n", end);
-	printf("\n============\n\n");
-	s = "		9223372036854775809   ";
-	printf("%jd\n", ft_strtoimax(s, &end, 0));
-	printf("end: %s\n", end);
-	printf("%jd\n", strtoimax(s, &end, 0));
-	printf("end: %s\n", end);
-	printf("\n============\n\n");
-	s = "		-9223372036854775809   ";
-	printf("%jd\n", ft_strtoimax(s, &end, 15));
-	printf("end: %s\n", end);
-	printf("%jd\n", strtoimax(s, &end, 15));
-	printf("end: %s\n", end);
-}
+// #include <inttypes.h>
+// int main()
+// {
+// 	char *s = "    321g2 ";
+// 	char *end = "";
+// 	printf("%jd\n", ft_strtoimax(s, &end, 4));
+// 	printf("end: %s\n", end);
+// 	printf("%jd\n", strtoimax(s, &end, 4));
+// 	printf("end: %s\n", end);
+// 	printf("\n============\n\n");
+// 	printf("%jd\n", ft_strtoimax(s, &end, 10));
+// 	printf("end: %s\n", end);
+// 	printf("%jd\n", strtoimax(s, &end, 10));
+// 	printf("end: %s\n", end);
+// 	printf("\n============\n\n");
+// 	s = "0x236f9   ";
+// 	printf("%jd\n", ft_strtoimax(s, &end, 15));
+// 	printf("end: %s\n", end);
+// 	printf("%jd\n", strtoimax(s, &end, 15));
+// 	printf("end: %s\n", end);
+// 	printf("\n============\n\n");
+// 	printf("%jd\n", ft_strtoimax(s, &end, 0));
+// 	printf("end: %s\n", end);
+// 	printf("%jd\n", strtoimax(s, &end, 0));
+// 	printf("end: %s\n", end);
+// 	printf("\n============\n\n");
+// 	s = "   -0x2A36E9   ";
+// 	printf("%jd\n", ft_strtoimax(s, &end, 16));
+// 	printf("end: %s\n", end);
+// 	printf("%jd\n", strtoimax(s, &end, 16));
+// 	printf("end: %s\n", end);
+// 	printf("\n============\n\n");
+// 	s = "		04434   ";
+// 	printf("%jd\n", ft_strtoimax(s, &end, 0));
+// 	printf("end: %s\n", end);
+// 	printf("%jd\n", strtoimax(s, &end, 0));
+// 	printf("end: %s\n", end);
+// 	printf("\n============\n\n");
+// 	s = "		9223372036854775809   ";
+// 	printf("%jd\n", ft_strtoimax(s, &end, 0));
+// 	printf("end: %s\n", end);
+// 	printf("%jd\n", strtoimax(s, &end, 0));
+// 	printf("end: %s\n", end);
+// 	printf("\n============\n\n");
+// 	s = "		-9223372036854775809   ";
+// 	printf("%jd\n", ft_strtoimax(s, &end, 15));
+// 	printf("end: %s\n", end);
+// 	printf("%jd\n", strtoimax(s, &end, 15));
+// 	printf("end: %s\n", end);
+// }
